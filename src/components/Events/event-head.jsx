78cee44cd668a355ac196f155de578906d6ed38b
PRANAW2004 <98792065+PRANAW2004@users.noreply.github.com>
2023-08-20 10:20:05 +0530

    Delete src directory

--- src/components/Events/event-head.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function EventHead(){
-    return(<div className="event1">
-        <h1 className="eventh1">Events</h1>
-    </div>)
-}
-export default EventHead;
--- src/components/Video-main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Videos from "./Videos/videos";
-function Videomain(){
-    return(<div>
-        <Videos />
-    </div>)
-}
-export default Videomain;
